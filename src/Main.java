11caf438f560b9918ff670a8ac8c55166445fe6e
Trin <trinity@ridley-smith.com>
2019-04-29 22:15:26 +1000

    Test Button and Listener Action

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
-
+    static int count = 0;
     public static void main(String[] args) {
         System.out.println("Opening....");
         JFrame jFrame = new JFrame("Leitner");
@@ -11,8 +13,18 @@ public class Main {
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
         JPanel mainPanel = new JPanel();
-        jFrame.setCon(mainPanel);
+        jFrame.getContentPane().add(mainPanel);
         JButton foo = new JButton("FOOOOOOOO");
         mainPanel.add(foo);
+        JLabel outputLabel = new JLabel("Yet");
+        mainPanel.add(outputLabel);
+        foo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("YEEEEEET");
+                outputLabel.setText(count+" counter");
+                count++;
+            }
+        });
     }
 }
