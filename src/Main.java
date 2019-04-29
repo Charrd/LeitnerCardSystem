929e7cb8234400cac0922197fbeef84b2b1d2061
Trin <trinity@ridley-smith.com>
2019-04-29 21:56:09 +1000

    First Button attempt

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Opening....");
         JFrame jFrame = new JFrame("Leitner");
+        jFrame.setSize(new Dimension(940,540));
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
+        JPanel mainPanel = new JPanel();
+        jFrame.setCon(mainPanel);
+        JButton foo = new JButton("FOOOOOOOO");
+        mainPanel.add(foo);
     }
 }
