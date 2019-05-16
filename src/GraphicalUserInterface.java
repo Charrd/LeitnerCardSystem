99e790d563bc63c2ab8923e660a0cbdec5a0348b
Trin <trinity@ridley-smith.com>
2019-05-16 11:52:00 +1000

    began the complicated button grid/border layout - created help button and cleared up some stub stuff not needed now

--- src/GraphicalUserInterface.java
+++ src/GraphicalUserInterface.java
@@ -6,48 +6,62 @@ import java.awt.event.ActionListener;
 public class GraphicalUserInterface {
     int count = 0;
     public void initialise(){
-        JFrame mainJFrame = new JFrame("Leitner");
+        //makes the window that opens, un-resizable, terminates program on close
+        JFrame mainJFrame = new JFrame("Shoebox 2.0 by Trinity");
         mainJFrame.setSize(new Dimension(940,540));
         mainJFrame.setResizable(false);
         mainJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        //panel creation
+        Container mainContentPane = mainJFrame.getContentPane();
         JPanel mainPanel = new JPanel(new BorderLayout());
-        JPanel secondPanel = new JPanel();
-        Container contentPane = mainJFrame.getContentPane();
-        contentPane.setLayout(new GridLayout(2,1));
-        contentPane.add(mainPanel);
-        contentPane.add(secondPanel);
+        JPanel buttonPanel = new JPanel(new GridLayout(1,2));
+        JPanel helpPanel = new JPanel(new BorderLayout());
+        JPanel subButtonPanel = new JPanel(new GridLayout(2,1));
 
-        JButton foo = new JButton("FOOOOOOOO");
-        secondPanel.add(foo);
-        JButton bar = new JButton("Help");
-        secondPanel.add(bar);
+        //working with adding and layouts
+        mainContentPane.setLayout(new GridLayout(2,1));
+        mainContentPane.add(mainPanel);
+        mainContentPane.add(subButtonPanel);
+        subButtonPanel.add(buttonPanel);
+        subButtonPanel.add(helpPanel);
+
+        //Buttons created and added to panels
+        JButton addCardButton = new JButton("Add Card");
+        buttonPanel.add(addCardButton);
+        JButton reviewButton = new JButton("Review");
+        buttonPanel.add(reviewButton);
+        JButton helpButton = new JButton("Help");
+        helpPanel.add(helpButton);
 
 
+        //Title
         JLabel mainTitle = new JLabel("Shoebox 2.0");
         mainTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        mainTitle.setFont(new Font(mainTitle.getFont().getName(), Font.PLAIN, 80));
         mainPanel.add(mainTitle, BorderLayout.CENTER);
 
-        JLabel testingLabel = new JLabel("Yet");
-        secondPanel.add(testingLabel);
+//        JLabel testingLabel = new JLabel("Yet");
+//        secondPanel.add(testingLabel);
 
-        foo.addActionListener(new ActionListener() {
+        //button actions place holders for now
+        addCardButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("YEEEEEET");
-                testingLabel.setText(count+" counter");
+            }
+        });
+
+        reviewButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("yet");
+//                testingLabel.setText(count+" counter");
                 count++;
             }
         });
 
-        bar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("yet");
-                testingLabel.setText(count+" counter");
-                count++;
-            }
-        });
+
         mainJFrame.setVisible(true);
     }
 }
